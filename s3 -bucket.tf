f6d71cd905dea7eae4ea5d67f6dbac4e3f37471a
sachinneena1021 <151525054+sachinneena1021@users.noreply.github.com>
2024-01-06 19:28:01 +0530

    Create s3 -bucket.tf

---
+++ s3 -bucket.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
+}
